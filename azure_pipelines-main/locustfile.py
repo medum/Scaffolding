0fd042c93051c3cff46778c836cc88392ee3a706
medum <49653011+medum@users.noreply.github.com>
2021-05-13 10:50:22 -0400

    Update locustfile.py

--- azure_pipelines-main/locustfile.py
+++ azure_pipelines-main/locustfile.py
@@ -1,38 +1,13 @@
-from flask import Flask
-server_port = 5000
-app = Flask(__name__)
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
-if __name__ == "__main__":
-    app.run('0.0.0.0',port=server_port)
-
-    from locust import HttpUser, TaskSet, task, between
+from locust import HttpUser, constant, task
 
 
-def index(l):
-    l.client.get("/")
+class MyReqRes(HttpUser):
 
+    wait_time = constant(1)
+    host = "https://flask-ml-azure.azurewebsites.net/"
 
-def stats(l):
-    l.client.get("/stats/requests")
-
-
-class UserTasks(TaskSet):
-    # one can specify tasks like this
-    tasks = [index, stats]
-
-    # but it might be convenient to use the @task decorator
     @task
-    def page404(self):
-        self.client.get("/does_not_exist")
-
-
-class WebsiteUser(HttpUser):
-    """
-    User class that does requests to the locust web server running on localhost
-    """
-
-    host = "http://127.0.0.1:5000"
-    wait_time = between(2, 5)
-    tasks = [UserTasks]
+    def get_users(self):
+        res = self.client.get("/")
+        print(res.status_code)
+        assert res.status_code == 200
